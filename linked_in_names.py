8620793255ae39daca5d40f67c8c3c4ae5e4d792
mfowl <46323283+mfowl@users.noreply.github.com>
2019-01-02 09:16:35 -0600

    Add files via upload

---
+++ linked_in_names.py
@@ -0,0 +1,12 @@
+# Print flow request in raw format. This can be used to record request fixtures
+import datetime
+import os
+import re
+
+
+def response(flow):
+    with open('/tmp/linked-in-names-%s.txt' % datetime.datetime.now().date(), "a") as f:
+        names = re.findall(r'firstName":"[a-zA-Z:",. ]+',flow.response.text)
+        for name in names:
+            clean = name.lstrip().rstrip().replace('firstName":"','').replace('","lastName":"',' ').replace('","','').lower()
+            f.write(clean + "\n")
